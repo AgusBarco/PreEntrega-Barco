c7c4d2480330726397c193c0b7cfe11991cbc31e
Agustin.Barco <agu.cba65@gmail.com>
2022-11-24 23:41:00 -0300

    PrimerCommit

--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -8,15 +8,12 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">OLIVIA</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarColor01">
+         <div className="collapse navbar-collapse" id="navbarColor01">
             <ul className="navbar-nav me-auto">
                 <Secciones/>
            
             </ul>
-            <Form/>
+            <Form busqueda= "Buscar producto"/>
           </div>
         </div>
       </nav>
